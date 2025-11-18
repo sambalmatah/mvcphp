8f3635f961b92ba926c0e2f8d8f710013acf53ef
sambalmatah <yudiarinugroho14@gmail.com>
2025-11-18 12:01:22 +0700

    pertemuan 02 routing

--- app/core/App.php
+++ app/core/App.php
@@ -5,7 +5,26 @@ class App {
     // coba class App sudah berjalan atau belum dengan construct
     public function __construct()
     {
-        echo 'Oke!';
+        // mengisi construct $url dengan method parseURL()
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    // mengambil URL kemudian memecah sesuai dengan keinginan
+    public function parseURL()
+    {
+        // cek jika ada URL
+        if( isset($_GET['url']) ) {
+            // parameter $url diisi dengan apa yang ditulis diURL 
+            // $url = $_GET['url'];
+            // tanpa '/' (garis miring) dengan rtrim
+            $url = rtrim($_GET['url'], '/');
+            // bersihkan karakter yang ngaco atau tidak sesuai
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            // pecah urlnya berdasarkan tanda '/'
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
 
--- app/init.php
+++ app/init.php
@@ -5,4 +5,7 @@ require_once 'core/App.php';
 // memanggil Class Controller
 require_once 'core/Controller.php';
 
+// membuat limit access terhadap file folder app dengan menggunakan file .
+// membuat limit access terhadap file folder public dengan menggunakan file .htaccess
+
 ?>
